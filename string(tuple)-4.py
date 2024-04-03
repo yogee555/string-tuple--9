a1738fafd1f181bde27a292c39891d53197a3688
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-04-03 06:52:35 +0530

    Add files via upload

---
+++ string(tuple)-4.py
@@ -0,0 +1,29 @@
+# Tuple
+tup1 = (100,"b","true","c","false")
+
+# find length of tuple
+print(len(tup1))
+
+# execute tuple
+print(tup1)
+
+# execute type of data
+print(type(tup1))
+
+# Extract Individual Element
+print(tup1[0])
+print(tup1[1])
+print(tup1[2])
+print(tup1[3])
+
+# Reverse
+print(tup1[-1])
+print(tup1[-2])
+print(tup1[-3])
+print(tup1[-4])
+print(tup1[-5])
+
+# join
+print(tup1[0:3])
+print(tup1[1:3])
+print(tup1[2:3])
